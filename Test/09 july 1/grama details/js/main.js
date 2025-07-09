6ed04b37936b14c131496a497d3283df3fc8c537
PKDLakshitha <155312677+Dinushan4568@users.noreply.github.com>
2025-07-09 14:47:04 +0530

    Initial commit
    Project scaffolding and initial file setup.

--- Test/09 july 1/grama details/js/main.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('logoutBtn').onclick = function() {
-  localStorage.clear();
-  window.location.href = 'index.html';
-};
-window.onload = function() {
-  if (localStorage.getItem('role') === 'admin') {
-    document.getElementById('promotionLink').style.display = 'block';
-  }
-};
--- Test/09 july 1/grama details/php/users.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db.php';
-$result = $conn->query("SELECT user_id, username, role FROM users");
-$rows = [];
-while ($row = $result->fetch_assoc()) $rows[] = $row;
-echo json_encode($rows);
-?>
