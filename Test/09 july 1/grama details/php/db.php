3153205f0d29264897bf52d7c1dc86293725a7b3
PKDLakshitha <155312677+Dinushan4568@users.noreply.github.com>
2025-07-20 10:31:41 +0530

    old done

--- Test/09 july 1/grama details/php/db.php
+++ Test/09 july 1/grama details/php/db.php
@@ -1,13 +1,12 @@
 <?php
 require_once 'cors.php';
-$serverName = "localhost\\SQLEXPRESS"; // Or your SQL Server instance name
-$connectionOptions = [
-    "Database" => "gramasewaka_db",
-    "Uid" => "your_sql_username", // <-- set your SQL Server username
-    "PWD" => "your_sql_password"  // <-- set your SQL Server password
-];
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if ($conn === false) {
+$host = 'localhost';
+$db = 'gramasewaka_db';
+$user = 'root';
+$pass = '';
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
     http_response_code(500);
-    die(json_encode(['error' => sqlsrv_errors()]));
+    die(json_encode(['error' => 'Database connection failed']));
 }
+?>
